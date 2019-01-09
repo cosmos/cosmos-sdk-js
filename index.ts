efbb2218adacf0eb6ca5274f3aa5a98a430d9226
文顺 章 <378253487@qq.com>
2019-01-09 10:19:09 +0800

    commit something

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import CosWeb from './src/index'
 const cosweb = new CosWeb({ baseUrl: '' })
 
 console.log(cosweb.config)
-
+// cosweb.http
 cosweb.Keys.getAll().then(res => {
     console.log('------------------')
     console.log(res.data)
